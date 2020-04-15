46b96b6f63347140a87a97355a1eb3e1de3a95bb
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2020-04-15 00:38:17 -0500

    update config

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "VmLl78572097" 
+
+class ProductionConfig(Config):
+    SECRET_KEY = os.environ['SECRET_KEY']
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
